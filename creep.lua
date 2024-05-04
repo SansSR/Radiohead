1a00f1ac0b8c60795f64476bca3ec35f3769b2b8
Sans <69919035+SansSR@users.noreply.github.com>
2024-05-04 12:17:20 -0400

    Update creep.lua
    forced ply to be male

--- creep.lua
+++ creep.lua
@@ -1,21 +1,25 @@
 ---@type Plugin
 local plugin = ...
-plugin.name = 'Creep'
+plugin.name = 'Radiohead'
 plugin.author = 'Sans'
 plugin.description = 'Turns player into Thom Yorke'
 
-plugin.commands['/creep'] = {
+plugin.commands['/radiohead'] = {
 	info = 'Turns you into Thom Yorke',
 	canCall = function (ply) return ply end,
 	call = function (ply, man, args)
 		assert(#args >= 0, 'usage')
     	local creep = ply
-	ply.human.head = 0
-    	ply.human.eyeColor = 6
-    	ply.human.hair = 6
-    	ply.human.hairColor = 4
-    	ply.human.skinColor = 2
-    	creep.human.lastUpdatedWantedGroup = -1
+	if ply.gender == 1 then
+		ply.human.head = 0
+    		ply.human.eyeColor = 6
+    		ply.human.hair = 6
+    		ply.human.hairColor = 4
+    		ply.human.skinColor = 2
+    		creep.human.lastUpdatedWantedGroup = -1
 		creep:sendMessage("You feel like a creep.")
+	else 
+		creep:sendMessage("Sorry, boys only!")
+	end
 end
 }
